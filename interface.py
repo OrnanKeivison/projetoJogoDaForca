b6ae396a540372b0ec7820f8b5f76618130cba50
ItsNabru <128171489+ItsNabru@users.noreply.github.com>
2023-05-22 10:22:13 -0300

    comecei a fazer a interface com turtle.

---
+++ interface.py
@@ -0,0 +1,33 @@
+import turtle
+turtle.speed(2) 
+turtle.si
+
+#desenha cabeça
+turtle.circle(50)
+
+#desenha os braços
+turtle.right(45)
+turtle.fd(100)
+turtle.pu()
+turtle.backward(100)
+turtle.pd()
+turtle.right(90)
+turtle.fd(100)
+
+turtle.pu()
+turtle.backward(100)
+turtle.right(-45)
+turtle.pd()
+turtle.fd(200)
+
+turtle.right(45)
+turtle.fd(100)
+turtle.pu()
+turtle.backward(100)
+turtle.pd()
+turtle.right(-90)
+turtle.fd(100)
+
+turtle.mainloop()
+
+turtle.hideturtle()
