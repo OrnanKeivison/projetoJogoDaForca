57d54ca8206f547c73fd9e07aea4151d39f628df
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-05-22 12:06:15 -0300

    polimento na interface

--- interface.py
+++ interface.py
@@ -1,33 +1,53 @@
 import turtle
-turtle.speed(2) 
-turtle.si
+turtle = turtle.Turtle()
+turtle.speed(1) 
 
+#posiciona tartaruga
+turtle.pu()
+turtle.goto(-212.13,212.13)
+turtle.right(-90)
+turtle.fd(50)
+turtle.pd()
+
+#desenha forca
+turtle.fd(25)
+turtle.right(-90)
+turtle.fd(100)
+turtle.left(90)
+turtle.fd(250)
+
+#posiciona tartaruga
+turtle.pu()
+turtle.goto(-212.13,212.13)
+turtle.left(90)
+turtle.pd()
 #desenha cabeça
-turtle.circle(50)
+turtle.circle(25)
 
 #desenha os braços
 turtle.right(45)
-turtle.fd(100)
+turtle.fd(50)
 turtle.pu()
-turtle.backward(100)
+turtle.backward(50)
 turtle.pd()
 turtle.right(90)
-turtle.fd(100)
+turtle.fd(50)
 
+#desenha o corpo
 turtle.pu()
-turtle.backward(100)
+turtle.backward(50)
 turtle.right(-45)
 turtle.pd()
-turtle.fd(200)
+turtle.fd(100)
 
 turtle.right(45)
-turtle.fd(100)
+turtle.fd(50)
 turtle.pu()
-turtle.backward(100)
+turtle.backward(50)
 turtle.pd()
 turtle.right(-90)
-turtle.fd(100)
+turtle.fd(50)
 
 turtle.mainloop()
 
-turtle.hideturtle()
+
